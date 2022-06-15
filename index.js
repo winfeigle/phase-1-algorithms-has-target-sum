5e158ec7666c2cc711d2c980e85ff4eaa86e7578
Win Feigle <win.feigle@gmail.com>
2022-06-14 21:29:59 -0600

    commit tree 9cb1a4bfbb48004d83b56d2574fe14fa0a507df0

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  if(array.length === 1) return false;
+  for(let i = 0; i <= array.length; i++){
+    for(let j = i + 1; j <= array.length; j++){
+      if(array[i] + array[j] === target){
+        return true;
+      }
+    }
+  }
+  return false;
 }
 
 /* 
